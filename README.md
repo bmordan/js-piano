f9e5660848c29e8dbf2418fcb92b46a299795288
Bernard Mordan <bernardmordan@gmail.com>
2022-02-16 16:32:29 +0000

    adds some images in the readme

--- README.md
+++ README.md
@@ -5,3 +5,9 @@
 Use `flex` to layout a piano keyboard.
 
 ![piano](https://user-images.githubusercontent.com/4499581/154289044-8fcf726d-9db3-4d7e-a749-fd4f6bc6ea78.png)
+
+[Watch part 2](https://user-images.githubusercontent.com/4499581/154311143-b580b3a0-b037-4cc5-8f2f-ddf63bc914b1.png)
+
+You will need to sort out my frequencies as I have made a mistake somewhere.
+
+![audio mapping](https://user-images.githubusercontent.com/4499581/154311143-b580b3a0-b037-4cc5-8f2f-ddf63bc914b1.png)
--- main.js
+++ main.js
@@ -1,8 +1,44 @@
+let context = new AudioContext()
+
+const frequencies = {
+    a: 261,
+    w: 277.2,
+    s: 293.7,
+    e: 311.1,
+    d: 329.6,
+    f: 349.2,
+    t: 370,
+    g: 392,
+    y: 415.3,
+    h: 440,
+    u: 466.2,
+    j: 493.9,
+    i: 523.3,
+    k: 587.3,
+    o: 622.3,
+    l: 659.3,
+    p: 698.5,
+    ";": 740,
+    "'": 784
+}
+
 function playNote(event) {
-    console.log("key pressed", event.key)
+    if (!frequencies[event.key]) return
     document.getElementById(event.key).classList.add('white-key-pressed')
+    const o = context.createOscillator()
+    const g = context.createGain()
+    o.type = "square"
+    g.gain.exponentialRampToValueAtTime(0.0001, context.currentTime + 1)
+    o.frequency.value = frequencies[event.key]
+    o.connect(g)
+    g.connect(context.destination)
+    o.start()
+    o.stop(context.currentTime + 1)
 }
 
 function clearNote(event) {
+    if (!frequencies[event.key]) return
+    if (Object.keys(frequencies).indexOf(event.key) < 0) return
     document.getElementById(event.key).classList.remove('white-key-pressed')
 }
+
